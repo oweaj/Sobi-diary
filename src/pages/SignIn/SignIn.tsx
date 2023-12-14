fbfbc42d155f429683d094d4cc2df43631a075dc
oweaj <qwe1520156@naver.com>
2023-12-14 17:03:43 +0900

    [FEAT] firebase 구글 로그인 및 로고 구성

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,7 +1,35 @@
+import { useState } from 'react';
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { auth } from '../../firebase';
+import { FcGoogle } from 'react-icons/fc';
+import { useNavigate } from 'react-router-dom';
+
 const SignIn = () => {
+  const [user, setUser] = useState<object | null>(null);
+  const navigate = useNavigate();
+
+  const handleGoogleLogin = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((data) => {
+        setUser(data.user);
+        navigate('/main');
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <div>
-      <div>로그인 페이지</div>
+    <div className="absolute center w-full">
+      <img src="/logo.png" className="object-cover" alt="한달소비 로고 이미지" />
+      <button
+        className="flex items-center justify-center gap-2 mx-auto font-semibold border rounded-md shadow-md py-1 px-6"
+        onClick={handleGoogleLogin}
+      >
+        <FcGoogle className="w-10 h-10" />
+        <span>Google Login</span>
+      </button>
     </div>
   );
 };
