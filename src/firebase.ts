cef8bd463d906f3e5da8c4eaa39b096dff62e86c
oweaj <qwe1520156@naver.com>
2023-12-14 17:06:37 +0900

    [SETTING] firebase config 정보 환경변수 설정

---
+++ src/firebase.ts
@@ -0,0 +1,27 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const {
+  VITE_API_KEY,
+  VITE_AUTH_DOMAIN,
+  VITE_PROJECT_ID,
+  VITE_STORAGE_BUCKET,
+  VITE_MESSAGING_SENDER_ID,
+  VITE_ID,
+  VITE_MEASUREMENT_ID,
+} = import.meta.env;
+
+const firebaseConfig = {
+  apiKey: VITE_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_STORAGE_BUCKET,
+  messagingSenderId: VITE_MESSAGING_SENDER_ID,
+  appId: VITE_ID,
+  measurementId: VITE_MEASUREMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
