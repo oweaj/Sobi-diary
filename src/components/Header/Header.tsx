50ce076ec88a180ff7946a30b59fd3ad283ec10e
oweaj <qwe1520156@naver.com>
2023-12-20 23:54:59 +0900

    [FIX] 타이틀 변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -17,8 +17,8 @@ const Header = () => {
   };
 
   return (
-    <div className="flex items-center justify-center p-3 border-b border-gray-300">
-      <h1 className="text-2xl font-medium">한 달 소비</h1>
+    <div className="flex items-center justify-center p-2 border-b border-gray-300">
+      <h1 className="text-[26px] font-bold">씀.</h1>
       <button type="button" className="absolute right-3 p-1" onClick={handleLogout}>
         <FiLogOut className="w-6 h-6" />
       </button>
