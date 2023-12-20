21ffd5e2d32901b9bdbcd34794a1861bd7ad03fd
oweaj <qwe1520156@naver.com>
2023-12-20 23:54:37 +0900

    [SETTING] getFirestore 추가

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const {
   VITE_API_KEY,
@@ -23,5 +24,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { app, auth };
+export { app, auth, db };
