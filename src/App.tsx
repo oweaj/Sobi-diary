9a3af2f91eab2757b317bcd1382bf3f6a6a63cc2
oweaj <qwe1520156@naver.com>
2023-12-21 23:47:25 +0900

    [STYLE] logo 파일 변경 및 스타일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Main from './pages/Main/Main';
 
 function App() {
   return (
-    <div className="fixed w-[450px] h-full left-1/2 -translate-x-1/2 border border-gray-300 rounded-2xl shadow-xl overflow-hidden">
+    <div className="fixed w-[25rem] h-full left-1/2 -translate-x-1/2 border border-gray-300 rounded-2xl shadow-xl overflow-hidden">
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/main" element={<Main />} />
