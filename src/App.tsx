68fecec1069709d522254102201000707e4e8359
oweaj <qwe1520156@naver.com>
2023-12-23 15:50:41 +0900

    [FEAT] 로그인 상태에 따른 페이지 리다이렉트

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import SignIn from './pages/SignIn/SignIn';
 import Main from './pages/Main/Main';
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      user ? navigate('main') : navigate('/');
+    });
+  }, []);
+
   return (
-    <div className="fixed w-[25rem] h-full left-1/2 -translate-x-1/2 border border-gray-300 rounded-2xl shadow-xl overflow-hidden">
+    <div className="fixed w-[25rem] h-full left-1/2 -translate-x-1/2 border border-gray-400 rounded-2xl shadow-xl overflow-hidden">
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="/main" element={<Main />} />
