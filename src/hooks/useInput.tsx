2a10f4adfba0791f7b917d072349dc01926e14c9
oweaj <qwe1520156@naver.com>
2023-12-26 12:35:07 +0900

    [FIX] 금액 input 숫자 수 제한으로 인한 정규식 적용

--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -1,13 +1,22 @@
 import { ChangeEvent, useState } from 'react';
 
-const useInput = () => {
+const useInput = (id: string) => {
   const [data, setData] = useState('');
+  const today = new Date().toISOString();
+  const [date, setDate] = useState(today);
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setData(e.target.value);
+    if (id === 'price') {
+      const onlyNumberValue = e.target.value.replace(/[^\d]/g, '');
+      setData(Number(onlyNumberValue).toLocaleString());
+    } else if (id === 'date') {
+      setDate(e.target.value);
+    } else {
+      setData(e.target.value);
+    }
   };
 
-  return { onChange, data };
+  return { onChange, data, date };
 };
 
 export default useInput;
